2b9893b2dfab382c27556f3a5eadaa11c62b2f90
ypg521 <ypg521@126.com>
2017-06-28 17:49:49 +0800

    tx

---
+++ ypg-test/src/main/java/com/ypg521/mybatis/TestMybatis.java
@@ -0,0 +1,7 @@
+package com.ypg521.mybatis;
+
+/**
+ * Created by Administrator on 2017/6/28.
+ */
+public class TestMybatis {
+}
---
+++ ypg-test/src/main/java/com/ypg521/tx/TestTx.java
@@ -0,0 +1,7 @@
+package com.ypg521.tx;
+
+/**
+ * Created by Administrator on 2017/6/28.
+ */
+public class TestTx {
+}
